5f83496edbd8848fb0d1805e37f7a30ddc3391da
Giacomo Mossio <giacomo.mossio@gmail.com>
2025-02-08 11:13:55 +0100

    oopsie2

--- maps/Introduction to GeoPandas/maps.py
+++
@@ -1,5 +0,0 @@
-import geopandas as gpd
-
-districts = gpd.read_file("Introduction to GeoPandas\Shapefiles\districts.shp")
-
-districts.plot()
--- maps/world/world.py
+++
@@ -1,6 +0,0 @@
-import geopandas
-import geoplot
-
-world = geopandas.read_file(
-    geopandas.datasets.get_path('naturalearth_lowres')
-)
